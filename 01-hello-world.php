8cd11a34256602a15d425fc275bb0c158659ff8b
Robb04 <cyrillerobbins@gmail.com>
2022-10-05 11:49:35 +0200

    première page php

---
+++ 01-hello-world.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello cf2m</title>
+</head>
+<body>
+    <?php // ouverture de php
+    // affiche la ligne de texte php
+    echo"<h1>Hello cf2m </h1>";
+    // fermeture de php ?>
+</body>
+</html>
