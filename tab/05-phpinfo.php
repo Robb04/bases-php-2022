2a78e346629202176eefef7f3290884476e81e30
Robb04 <cyrillerobbins@gmail.com>
2022-10-07 12:08:26 +0200

    exercice

--- tab/05-phpinfo.php
+++ tab/05-phpinfo.php
@@ -1,6 +1,9 @@
 <?php
+<<<<<<< HEAD
 // ceci affiche la configuration complète du serveur PHP / APACHE / SQL  + bibliothèques
 phpinfo();
 ?>
+=======
 // affiche la configuration complète du serveur PHP / APACHE / SQL + bibliothèques
 phpinfo();
+>>>>>>> 504ac4e886e3eb338a283e8f0b51c562cf70c4fe
